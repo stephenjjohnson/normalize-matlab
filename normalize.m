e23cbaaaaee61bf25f66f8a1bf6fb663edad17e3
Stephen Johnson <stephenjjohnson@gmail.com>
2016-04-25 09:39:38 -0500

    Added GPL-3 license

--- normalize.m
+++ normalize.m
@@ -1,3 +1,20 @@
+%    MatLab function to normalize data betreen arbitrary bounds
+%    Copyright (C) 2016 Stephen J. Johnson
+%
+%    This program is free software: you can redistribute it and/or modify
+%    it under the terms of the GNU General Public License as published by
+%    the Free Software Foundation, either version 3 of the License, or
+%    (at your option) any later version.
+%
+%    This program is distributed in the hope that it will be useful,
+%    but WITHOUT ANY WARRANTY; without even the implied warranty of
+%    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%    GNU General Public License for more details.
+%
+%    You should have received a copy of the GNU General Public License
+%    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
 function [normData] = normalize (myData, minData, maxData, minNorm, maxNorm )
 % NORMALIZE 
 % Normalizes myData from minData-maxData to the range minNorm-maxNorm
